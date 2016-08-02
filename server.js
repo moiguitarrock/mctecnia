1f18084bf53f3d27b0a7d2806e2faba9472ee8cf
moiguitarrock <maccma.c2@gmail.com>
2016-08-02 11:08:00 -0500

    initial configuration express server

---
+++ server.js
@@ -0,0 +1,19 @@
+var express = require('express');
+var app = express();
+var path = require('path');
+
+app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname+'/src/pages/home.html'));
+});
+
+app.get('/about', function (req, res) {
+  res.sendFile(path.join(__dirname+'/src/pages/about.html'));
+});
+
+app.get('/services', function (req, res) {
+  res.sendFile(path.join(__dirname+'/src/pages/services.html'));
+});
+
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!');
+});
