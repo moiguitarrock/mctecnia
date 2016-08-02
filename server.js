e185e2e1cb298a302ec6c5c264f59be5bdd993de
moiguitarrock <maccma.c2@gmail.com>
2016-08-02 16:39:15 -0500

    include the port

--- server.js
+++ server.js
@@ -14,6 +14,7 @@ app.get('/services', function (req, res) {
   res.sendFile(path.join(__dirname+'/src/pages/services.html'));
 });
 
-app.listen(3000, function () {
+app.listen(process.evn.PORT || 3000, function () {
   console.log('Example app listening on port 3000!');
+  console.log(process.env.PORT);
 });
