c30ff1f252cc982facb6b1d52ae0a8408ca39824
moiguitarrock <maccma.c2@gmail.com>
2016-08-02 16:50:36 -0500

    fix env variable

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.get('/services', function (req, res) {
   res.sendFile(path.join(__dirname+'/src/pages/services.html'));
 });
 
-app.listen(process.evn.PORT || 3000, function () {
+app.listen(process.env.PORT || 3000, function () {
   console.log('Example app listening on port 3000!');
   console.log(process.env.PORT);
 });
